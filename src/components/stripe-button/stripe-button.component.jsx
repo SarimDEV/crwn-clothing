f68e821a39dc493737e9c2426ef9ecbb1aafd574
SarimDEV <m2sarim.dev@gmail.com>
2020-08-28 16:12:28 -0400

    favicon and title changes and adding stripe components (no backend routing yet)

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HLDbBEtCUrQXlUT5EFUNL9vIhTxBxfZKf7a8XdfJ9Rdy1wlPpf5wEzexGa9nGvDmErRIbnmiZ5sanMqxhRRCl8G00l3veR8cG";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $ ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
